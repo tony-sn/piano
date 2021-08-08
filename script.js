5f0e4e3831a334988283543a5e638bdcf3b0e6bf
tung_nguyen12 <tony.sn@outlook.com>
2021-08-08 10:58:44 +1000

    add js code

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const audioContext = new AudioContext()
+
 const NOTE_DETAILS = [
   { note: "C", key: "Z", frequency: 261.626 },
   { note: "Db", key: "S", frequency: 277.183 },
@@ -12,3 +14,58 @@ const NOTE_DETAILS = [
   { note: "Bb", key: "J", frequency: 466.164 },
   { note: "B", key: "M", frequency: 493.883 },
 ]
+
+document.addEventListener("keydown", e => {
+  if (e.repeat) return
+  const keyboardKey = e.code //  Ex: keydown = z => code:" KeyZ"
+  const noteDetail = getNoteDetail(keyboardKey)
+
+  if (noteDetail == null) return
+
+  noteDetail.active = true
+  playNotes()
+})
+
+document.addEventListener("keyup", e => {
+  const keyboardKey = e.code
+  const noteDetail = getNoteDetail(keyboardKey)
+
+  if (noteDetail == null) return
+
+  noteDetail.active = false
+  playNotes()
+})
+
+function getNoteDetail(keyboardKey) {
+  return NOTE_DETAILS.find(
+    note_detail => `Key${note_detail.key}` === keyboardKey
+  )
+}
+
+function playNotes() {
+  NOTE_DETAILS.forEach(n => {
+    const keyElement = document.querySelector(`[data-note=${n.note}]`)
+    keyElement.classList.toggle("active", n.active || false)
+    if (n.oscillator != null) {
+      n.oscillator.stop()
+      n.oscillator.disconnect()
+    }
+  })
+
+  const activeNotes = NOTE_DETAILS.filter(n => n.active)
+  const gain = 1 / activeNotes.length
+  activeNotes.forEach(n => {
+    startNote(n, gain)
+  })
+}
+
+function startNote(noteDetail, gain) {
+  const gainNode = audioContext.createGain()
+  gainNode.gain.value = gain
+  const oscillator = audioContext.createOscillator()
+  oscillator.frequency.value = noteDetail.frequency
+  oscillator.type = "triangle"
+  oscillator.connect(gainNode).connect(audioContext.destination)
+  oscillator.start()
+  noteDetail.oscillator = oscillator
+}
